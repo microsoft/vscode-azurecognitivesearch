3f9b52f3bc3043947f06355a6efadeadd9a101db
Derek Legenzoff <dereklegenzoff@gmail.com>
2020-09-09 17:34:14 -0700

    adding in test folder and updating azure pipeline

---
+++ test/suite/global.test.ts
@@ -0,0 +1,20 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE.md in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import * as vscode from 'vscode';
+import { TestOutputChannel, TestUserInput } from 'vscode-azureextensiondev';
+import { ext } from '../../extension.bundle';
+
+// tslint:disable-next-line:strict-boolean-expressions export-name
+export let longRunningTestsEnabled: boolean = !/^(false|0)?$/i.test(process.env.ENABLE_LONG_RUNNING_TESTS || '');
+export const testUserInput: TestUserInput = new TestUserInput(vscode);
+
+// Runs before all tests
+suiteSetup(async function (this: Mocha.Context): Promise<void> {
+    this.timeout(2 * 60 * 1000);
+    await vscode.commands.executeCommand('azureCognitiveSearch.refresh'); // activate the extension before tests begin
+    ext.outputChannel = new TestOutputChannel();
+    ext.ui = testUserInput;
+});
