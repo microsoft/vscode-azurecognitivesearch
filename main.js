1d8a998d365a6d11804d47e71bba361f575fed37
Derek Legenzoff <dereklegenzoff@gmail.com>
2020-09-15 11:21:11 -0700

    removing commented code from delegenz/tests

--- main.js
+++ main.js
@@ -18,10 +18,6 @@ Object.defineProperty(exports, "__esModule", {
     value: true
 });
 
-//const ignoreBundle = !/^(false|0)?$/i.test(process.env.AZCODE_SEARCH_IGNORE_BUNDLE || '');
-//const extensionPath = ignoreBundle ? "./dist/extension.bundle" : "./dist/extension.bundle";
-//const extension = require(extensionPath);
-
 const extension = require('./dist/extension.bundle');
 
 async function activate(ctx) {
